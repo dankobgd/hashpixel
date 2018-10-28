9cd20a4a7b255c67a54f7e4a06d96cad399d200e
Danko Petrovic <dp24031995@gmail.com>
2018-10-28 19:48:32 +0100

    afterResponsive cb

--- client/src/fullpage/fullpageOptions.js
+++ client/src/fullpage/fullpageOptions.js
@@ -3,7 +3,7 @@ import items from '../navigation/items';
 const anchors = items.map(item => item.name);
 
 export default {
-  callbacks: ['onLeave'],
+  callbacks: ['onLeave', 'afterResponsive'],
   licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
   anchors,
   menu: '.dotsList',
@@ -11,7 +11,4 @@ export default {
   navigationPosition: 'left',
   scrollOverflow: true,
   responsiveWidth: 980,
-  afterResponsive: (isResponsive) => {
-    console.log(`Is responsive: ${isResponsive}`);
-  }
 };
