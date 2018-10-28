7983039e2a3723aeeafedbafe9c0101c6ba92931
Danko Petrovic <dp24031995@gmail.com>
2018-10-28 21:43:21 +0100

    separate sections component refactor

--- client/src/fullpage/FullpageWrapper.js
+++ client/src/fullpage/FullpageWrapper.js
@@ -2,27 +2,9 @@ import React, { Component } from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 import '../../node_modules/fullpage.js/vendors/scrolloverflow';
 import fullpageOptions from './fullpageOptions';
-import { HeroSection, AboutSection, MeetOurTeamSection, ServicesSection, ContactSection, Footer, Empty } from '../sections';
 import Navigation from '../navigation/Navigation';
+import Sections from '../sections';
 import Bullets from '../bullets/Bullets'
-import { toggleFooter } from '../helpers';
-
-const Sections = ({ fpState, fpApi, footerUILogic }) => {
-  toggleFooter(fpState, footerUILogic);
-
-  return (
-    <div style={{ position: 'relative' }}>
-      <HeroSection fpApi={fpApi}/>
-      <AboutSection />
-      <MeetOurTeamSection />
-      <ServicesSection />
-      <ContactSection />
-      <Empty />
-      <Footer fpState={fpState} fpApi={fpApi} isFooterOpen={footerUILogic.isFooterOpen}/>
-    </div>
-  )
-}
-
 
 class FullPageWrapper extends Component {
   state = {
--- client/src/sections/index.js
+++ client/src/sections/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import HeroSection from './Hero';
 import AboutSection from './About';
 import MeetOurTeamSection from './MeetOurTeam';
@@ -5,6 +6,21 @@ import ServicesSection from './Services';
 import ContactSection from './Contact';
 import Footer from './Footer';
 import Empty from './Empty';
+import { toggleFooter } from '../helpers'
 
 
-export { HeroSection, AboutSection, MeetOurTeamSection, ServicesSection, ContactSection, Footer, Empty }
+export default ({ fpState, fpApi, footerUILogic }) => {
+  toggleFooter(fpState, footerUILogic);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <HeroSection fpApi={fpApi}/>
+      <AboutSection />
+      <MeetOurTeamSection />
+      <ServicesSection />
+      <ContactSection />
+      <Empty />
+      <Footer fpState={fpState} fpApi={fpApi} isFooterOpen={footerUILogic.isFooterOpen}/>
+    </div>
+  )
+}
