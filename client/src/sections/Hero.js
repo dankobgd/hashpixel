712a0a2caed5365d02fc117110a631ec5852876d
Danko Petrovic <dp24031995@gmail.com>
2018-10-29 15:38:48 +0100

    add scroll svg animation

--- client/src/sections/Hero.js
+++ client/src/sections/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Logo from '../images/Logo';
 import { Container, Section } from '../shared/style';
 import bgImage from '../images/bg-home.png'
@@ -74,11 +74,29 @@ const ScrollDown = styled.div`
   }
 `;
 
-const ArrowDown = () => (
-  <svg version="1.1" id="Layer_1" x="0px" y="0px" width="15.672px" height="12.406px" viewBox="0 0 15.672 12.406">
-    <polyline fill="none" stroke="#6A8080" strokeMiterlimit="10" points="0.344,5.406 7.844,11.406 15.344,5.406 " />
-    <polyline fill="none" stroke="#6A8080" strokeMiterlimit="10" points="15.344,0.406 7.844,6.406 0.344,0.406 " />
-  </svg>
+
+
+
+const scrollAnimation = keyframes`
+  0% {
+		transform: translateY(0);
+	}
+	30% {
+		transform: translateY(50px);
+	}
+`;
+
+const Svg = styled.svg`
+  & path#wheel {
+    animation: ${scrollAnimation} 2s ease infinite;
+  }
+`;
+
+const Mouse = () => (
+  <Svg width="30px" height="100%" viewBox="0 0 247 390" version="1.1" xmlns="http://www.w3.org/2000/svg" style={{fillRule:'evenodd',clipRule:'evenodd',strokeLinecap:'round',strokeLinejoin:'round',strokeMiterlimit:'1.5'}}>
+	<path id="wheel" d="M123.359,79.775l0,72.843" style={{fill:'none',stroke:'#fff',strokeWidth:'20px'}}/>
+	<path id="mouse" d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z" style={{ fill:'none',stroke:'#fff',strokeWidth:'20px'}}/>
+</Svg>
 );
 
 
@@ -93,7 +111,7 @@ const HeroSection = ({ fpApi }) => (
       </Banner>
 
       <ScrollDown onClick={() => fpApi.moveSectionDown()}>
-        <ArrowDown />
+        <Mouse />
         <span>scroll</span>
       </ScrollDown>
     </Container>
