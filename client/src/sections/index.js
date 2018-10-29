594070dce89ec1a59bfdd401e810d8bcd5d7fe76
Danko Petrovic <dp24031995@gmail.com>
2018-10-29 13:41:24 +0100

    remove warning

--- client/src/sections/index.js
+++ client/src/sections/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HeroSection from './Hero';
 import AboutSection from './About';
 import MeetOurTeamSection from './MeetOurTeam';
@@ -9,18 +9,25 @@ import Empty from './Empty';
 import { toggleFooter } from '../helpers'
 
 
-export default ({ fpState, fpApi, footerUILogic }) => {
-  toggleFooter(fpState, footerUILogic);
+export default class Sections extends Component {
+  componentDidUpdate(prevProps) {
+    const { fpState, footerUILogic } = prevProps;
+    toggleFooter(fpState, footerUILogic);
+  }
 
-  return (
-    <div style={{ position: 'relative' }}>
-      <HeroSection fpApi={fpApi}/>
-      <AboutSection />
-      <MeetOurTeamSection />
-      <ServicesSection />
-      <ContactSection />
-      <Empty />
-      <Footer fpState={fpState} fpApi={fpApi} isFooterOpen={footerUILogic.isFooterOpen}/>
-    </div>
-  )
+  render() {
+    const { fpState, fpApi, footerUILogic: { isFooterOpen } } = this.props;
+
+    return (
+      <div style={{ position: 'relative' }}>
+        <HeroSection fpApi={fpApi}/>
+        <AboutSection />
+        <MeetOurTeamSection />
+        <ServicesSection />
+        <ContactSection />
+        <Empty />
+        <Footer fpState={fpState} fpApi={fpApi} isFooterOpen={isFooterOpen}/>
+      </div>
+    )
+  }
 }
