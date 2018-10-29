741bf4e4c400e8b92abff5d143f1a758a7e64be0
Danko Petrovic <dp24031995@gmail.com>
2018-10-29 19:51:26 +0100

    fix spinner

--- client/src/App.js
+++ client/src/App.js
@@ -4,38 +4,23 @@ import FullPageWrapper from './fullpage/FullpageWrapper'
 import PageNotFound from './pages/PageNotFound';
 import { ThemeProvider } from 'styled-components';
 import  { mainTheme } from './shared/theme'
-import Loader from './shared/Loader';
 import './App.css';
 
 
-class App extends Component {
-  state = { loading: true }
-
-  componentDidMount(){
-    this.setState({ loading: false })
-  }
-
-  render() {
-    return (
-      <>
-        {this.state.loading ? <Loader /> : (
-        <ThemeProvider theme={mainTheme}>
-          <Router>
-            <Switch>
-              <Route
-                exact
-                path="/"
-                component={FullPageWrapper}
-              />
-              <Route component={PageNotFound} />
-            </Switch>
-          </Router>
-        </ThemeProvider>
-        )}
-      </>
-    )
-  }
-}
+const App = () => (
+  <ThemeProvider theme={mainTheme}>
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={FullPageWrapper}
+        />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
+  </ThemeProvider>
+)
 
 
 export default App;
