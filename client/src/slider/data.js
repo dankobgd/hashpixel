0439905edae791bf3ddb50c85f44a4307ab824d7
Danko Petrovic <dp24031995@gmail.com>
2018-11-07 18:10:43 +0100

    change imgs cuz copyright

--- client/src/slider/data.js
+++ client/src/slider/data.js
@@ -2,31 +2,31 @@ export  default [
   {
     title: 'Design',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi laboriosam dolor repudiandae amet maiores voluptate ratione? Doloremque, aspernatur. Mollitia atque adipisci eum dolorum. Non facere natus eos perferendis voluptate.',
-    imageUrl: 'http://codexive.rs/wp-content/uploads/2018/05/Social-media-marketing.png'
+    imageUrl: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3f6e5055d9ad1d603fd364c11823d026&auto=format&fit=crop&w=500&q=60'
   },
   {
     title: 'Identity',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi laboriosam dolor repudiandae amet maiores voluptate ratione? Doloremque, aspernatur. Mollitia atque adipisci eum dolorum. Non facere natus eos perferendis voluptate.',
-    imageUrl: 'http://codexive.rs/wp-content/uploads/2018/05/Web-Development.png'
+    imageUrl: 'https://images.unsplash.com/photo-1495639867387-5423d6811583?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8cf6335dfe08a50fa199c96e84181b4e&auto=format&fit=crop&w=500&q=60'
   },
   {
     title: 'Dev',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi laboriosam dolor repudiandae amet maiores voluptate ratione? Doloremque, aspernatur. Mollitia atque adipisci eum dolorum. Non facere natus eos perferendis voluptate.',
-    imageUrl: 'http://codexive.rs/wp-content/uploads/2018/05/UI-UX-Web_design.png'
+    imageUrl: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=08bcfbcc8c59a469f6a438a595886e4d&auto=format&fit=crop&w=500&q=60'
   },
   {
     title: 'Host',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi laboriosam dolor repudiandae amet maiores voluptate ratione? Doloremque, aspernatur. Mollitia atque adipisci eum dolorum. Non facere natus eos perferendis voluptate.',
-    imageUrl: 'http://codexive.rs/wp-content/uploads/2018/05/SEO-PPC.png'
+    imageUrl: 'https://images.unsplash.com/photo-1518186285589-2f7649de83e0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=eb99de7c333b241530872220aa87e93c&auto=format&fit=crop&w=500&q=60'
   },
   {
     title: 'Mark',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi laboriosam dolor repudiandae amet maiores voluptate ratione? Doloremque, aspernatur. Mollitia atque adipisci eum dolorum. Non facere natus eos perferendis voluptate.',
-    imageUrl: 'http://codexive.rs/wp-content/uploads/2018/05/Social-media-marketing.png'
+    imageUrl: 'https://images.unsplash.com/photo-1508896080210-93c377eb4135?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e3dc0a18289acbaff5c9908fa252f982&auto=format&fit=crop&w=500&q=60'
   },
   {
     title: 'SEO',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eligendi laboriosam dolor repudiandae amet maiores voluptate ratione? Doloremque, aspernatur. Mollitia atque adipisci eum dolorum. Non facere natus eos perferendis voluptate.',
-    imageUrl: 'http://codexive.rs/wp-content/uploads/2018/05/Web-Development.png'
+    imageUrl: 'https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fe27841e0f0f2922406eb0df58430433&auto=format&fit=crop&w=500&q=60'
   },
 ]
